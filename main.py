a34537da6f24e19f8a72e65af8d3ddf5e86c4985
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-07-27 17:04:01 +0100

    get number of lines function half written

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# CONSTANTS DEFINITION using capital  letters for my variable which is a conventional way for declaring python constants
+MAX_LINES =3 
+
 # defining function to accept users deposit.
 
 def deposit():
-    while True:
 
+    while True:
         amount = input("What amount do you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
@@ -14,7 +17,25 @@ def deposit():
             print("Please enter a number.")
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter number of lines to bet on(1 -"+ str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if lines > 0:
+                break
+            else: 
+                print("Amount must be greater than zero")
+        else:
+            print("Please enter a number.")
+    return amount
+
+
+def main():
+
+    balance = deposit()
+
+    main()
     
 
   
