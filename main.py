956f0b11d143f2c2d05313ab9dac912c41d58271
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-07-28 00:00:53 +0100

    get bet function completed

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # CONSTANTS DEFINITION using capital  letters for my variable which is a conventional way for declaring python constants
 MAX_LINES =3 
+MAX_BET = 100
+MIN_BET =1
 
-# defining function to accept users deposit.
-
+# defining function to accept players deposit.
 def deposit():
 
     while True:
-        amount = input("What amount do you like to deposit? $")
+        amount = input("What amount do you like deposit ? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print("Amount must be greater than zero")
+                print(f"Amount must be between greeter than zero")
         else:
             print("Please enter a number.")
     return amount
@@ -33,13 +34,46 @@ def get_number_of_lines():
     return lines
 
 
+
+
+def get_bet():
+
+    while True:
+        amount = input("What amount do you like to bet on each line ? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET<=amount<=MAX_BET :
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number.")
+    return amount
+
 def main():
 
     balance = deposit()
 
     lines = get_number_of_lines()
 
-    print(f"Balance :{balance}, Number of lines:{lines}")
+    bet = get_bet()
+
+    total_bet = bet * lines
+
+    while True:
+        if total_bet > balance:
+            print(f"You don't have sufficient balance to bet, your total balance is {balance}")
+        else:
+            break
+
+    
+
+    print(f"You are betting ${bet} on ${lines} lines, total bet is equal to ${total_bet} ")
+    
+
+  
+
+   
 
 main()
     
